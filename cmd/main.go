c9b3d6c2a539e91f4e17bbecbd1f57f4bb86eb19
Duan <duan.wang93@gmail.com>
2024-08-30 12:33:56 -0400

    add lambda handler

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+
+	"github.com/aws/aws-lambda-go/lambda"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
@@ -14,15 +17,30 @@ import (
 
 func main() {
 
-	//TODO: Add lambda handler
-
 	sdkConfig, err := config.LoadDefaultConfig(context.Background()) //TODO: check how config load works
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
 	dynamoDbClient := dynamodb.NewFromConfig(sdkConfig)
 
-	api.UpdateTable(dynamoDbClient, "visitor_counter") // TODO: replace table-name with the actual table name, belongs in lambda handler
+	h := Handler{DynamoDBClient: dynamoDbClient}
+
+	lambda.Start(h.HandleRequest)
 
 	log.SetFlags(0)
+}
+
+type Handler struct {
+	DynamoDBClient *dynamodb.Client
+}
+
+func (h *Handler) HandleRequest(ctx context.Context) (*string, error) {
+	response, err := api.UpdateTable(h.DynamoDBClient, "visitor_counter")
+
+	if err != nil {
+		return nil, err
+	}
+
+	message := fmt.Sprintf("num_visitors: %d!", response["num_visitors"])
+	return &message, nil
 }
