a333a8116bd69609db7e1e6a2bab1e3d77650ce5
Duan <duan.wang93@gmail.com>
2024-11-15 22:34:38 -0500

    add api gateway infra

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.74.0"
+    }
+  }
 
   backend "s3" {
     bucket         = "duan-production-tf-state-us-east-2"
--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -98,7 +98,7 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 
 # File is zipped separately in CICD pipeline
 resource "aws_lambda_function" "visitor_counter" {
-  # TODO: should pass the path to the zipped file as a variable
+  # TODO: should pass the path to the zipped file as a variable, or upload to s3
   filename      = "${path.module}/deployment.zip"
   function_name = "visitor_counter"
   role          = aws_iam_role.visitor_counter_lambda_role.arn
@@ -106,8 +106,85 @@ resource "aws_lambda_function" "visitor_counter" {
   runtime       = "provided.al2023"
   architectures = ["arm64"]
   # Sanity check to make sure the policy is set up
+  # TODO: Add another dependency on the existence of the published lambda code in the s3 bucket if possible
   depends_on = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
+
+# File is zipped separately in CICD pipeline
+resource "aws_lambda_function" "query_visitor_counter" {
+  # TODO: should pass the path to the zipped file as a variable, or upload to s3
+  filename      = "${path.module}/deployment.zip"
+  function_name = "query_visitor_counter"
+  role          = aws_iam_role.visitor_counter_lambda_role.arn
+  handler       = "hello.handler"
+  runtime       = "provided.al2023"
+  architectures = ["arm64"]
+  # Sanity check to make sure the policy is set up
+  # TODO: Add another dependency on the existence of the published lambda code in the s3 bucket if possible
+  depends_on = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
+resource "aws_apigatewayv2_api" "visitor_counter" {
+  name          = "visitor_counter-API"
+  protocol_type = "HTTP"
+  cors_configuration {
+    # TODO: Replace with origin
+    allow_origins = ["*"]
+    allow_methods = ["GET", "POST", "OPTIONS"]
+    allow_headers = ["Content-Type"]
+  }
+}
+
+resource "aws_apigatewayv2_integration" "query_visitor_counter" {
+  api_id                 = aws_apigatewayv2_api.visitor_counter.id
+  integration_type       = "AWS_PROXY"
+  integration_method     = "GET"
+  integration_uri        = aws_lambda_function.query_visitor_counter.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_integration" "visitor_counter" {
+  api_id                 = aws_apigatewayv2_api.visitor_counter.id
+  integration_type       = "AWS_PROXY"
+  integration_method     = "POST"
+  integration_uri        = aws_lambda_function.visitor_counter.invoke_arn
+  payload_format_version = "2.0"
+}
+
+
+resource "aws_apigatewayv2_route" "query_visitor_counter_lambda" {
+  api_id    = aws_apigatewayv2_api.visitor_counter.id
+  route_key = "GET /visitor_counter"
+  target    = "integrations/${aws_apigatewayv2_integration.query_visitor_counter.id}"
+}
+
+resource "aws_apigatewayv2_route" "visitor_counter_lambda" {
+  api_id    = aws_apigatewayv2_api.visitor_counter.id
+  route_key = "POST /visitor_counter"
+  target    = "integrations/${aws_apigatewayv2_integration.visitor_counter.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.visitor_counter.id
+  name        = "default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id = "AllowAPIGatewayInvoke"
+  action       = "lambda:InvokeFunction"
+  for_each = {
+    visitor_counter_lambda       = aws_lambda_function.visitor_counter
+    query_visitor_counter_lambda = aws_lambda_function.query_visitor_counter
+  }
+  function_name = each.value.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_apigatewayv2_api.visitor_counter.execution_arn}/*/*/visitor_counter"
+}
+
 
 # TODO: Pull in the account name in the bucket name from a data source with the account id
 # resource "aws_s3_bucket_website_configuration" "resume-site-prod-992382573336-us-east-2" {
